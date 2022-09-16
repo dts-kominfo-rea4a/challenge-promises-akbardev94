a8ee76ef0aee3a7e0ee6357bf52a17968da1378b
AnasMA94 <anas@labelideas.co>
2022-09-16 21:40:48 +0700

    challenge-promises

--- promise.js
+++ promise.js
@@ -1,7 +1,31 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise(
+    (resolve, reject) => {
+      const out = (async()=>{
+        const dataIXX = await promiseTheaterIXX();
+        const dataVGC = await promiseTheaterVGC();
+        let total = 0;
+        // hitung emosi dari dataIXX
+        for(var i in dataIXX) {
+          if(dataIXX[i].hasil == emosi){
+            total++;
+          }
+        }
+        // hitung emosi dari dataVGC
+        for(var i in dataVGC) {
+          if(dataVGC[i]. hasil == emosi){
+            total++
+          }
+        }
+        return(total)
+      })();
+      resolve(out)
+    }
+  );
+};
 
 module.exports = {
   promiseOutput,
