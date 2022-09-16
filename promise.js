935a471a7664022248821ac26439cc43e0d716e5
AnasMA94 <anas@labelideas.co>
2022-09-16 22:03:43 +0700

    Update Promises.js

--- promise.js
+++ promise.js
@@ -1,31 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = (emosi) => {
-  return new Promise(
-    (resolve, reject) => {
-      const out = (async()=>{
-        const dataIXX = await promiseTheaterIXX();
-        const dataVGC = await promiseTheaterVGC();
-        let total = 0;
-        // hitung emosi dari dataIXX
-        for(var i in dataIXX) {
-          if(dataIXX[i].hasil == emosi){
-            total++;
-          }
-        }
-        // hitung emosi dari dataVGC
-        for(var i in dataVGC) {
-          if(dataVGC[i]. hasil == emosi){
-            total++
-          }
-        }
-        return(total)
-      })();
-      resolve(out)
+const promiseOutput = async (params) => {
+  const merged = [...await promiseTheaterIXX(), ...await promiseTheaterVGC()];
+  let total = 0;
+  merged.forEach(hitung => {
+    if (hitung.hasil === params) {
+      total++;
     }
-  );
+  });
+  return total;
 };
+
 
 module.exports = {
   promiseOutput,
